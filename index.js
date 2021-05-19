cbd6566a30d1a4f1c65f2bb873fe18523a0ce627
lavish.mantri <lavish.mantri@thoughtspot.com>
2021-05-19 15:03:15 +0530

    setup rtc offer and ice candidate exchange handlers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const { disconnect } = require("cluster");
 
 const app = express();
 const server = http.createServer(app);
@@ -13,14 +14,42 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+const connectedUsers = {};
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+io.on("connection", (socket) => {
+  console.log("a user connected: ", socket.id);
+  socket.broadcast.emit('user_connected', socket.id);
+  io.allSockets().then(socketids => {
+    console.log('ALL socket ids : ', socketids.values());
+    socket.emit('all_users', [...socketids]);
   });
 
-  socket.on("message", (data) => console.log("Data recieved: ", data));
+  socket.on('disconnecting', () => {
+    console.log('disconnecting ', socket.id); // the Set contains at least the socket ID
+    socket.broadcast.emit('user_disconnecting', socket.id);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected: ", socket.id);
+  });
+
+  socket.on("message", (data) => {
+    console.log("Data recieved: ", data, socket.id);
+
+    socket.broadcast.emit("message", data);
+  });
+
+  socket.onAny((eventName, ...args) => console.log('Uncatched :', eventName, args));
+
+  socket.on('connection-offer', (data) => {
+    console.log('connection-offer: ', data);
+    socket.broadcast.emit('connection-offer', data);
+  });
+
+  socket.on('new-ice-candidate', data => {
+    console.log('new-ice-candidate: ', data);
+    socket.broadcast.emit('new-ice-candidate', data);
+  })
 });
 
 server.listen(8000, () => {
---
+++ signalling-server.js
@@ -0,0 +1,6 @@
+
+const handlers = {
+    'connection-offer': () => {
+        
+    }
+}
